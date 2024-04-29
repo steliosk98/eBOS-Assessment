75999025583d562b49b5ee2bdd60c172f22d4159
steliosk98 <37410811+steliosk98@users.noreply.github.com>
2024-04-29 22:43:38 +0300

    added pagination and modified rows view

--- react-users-app/src/Users.js
+++ react-users-app/src/Users.js
@@ -3,6 +3,8 @@ import { FormattedMessage } from 'react-intl';
 
 function Users() {
     const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage] = useState(10);
 
     useEffect(() => {
         fetch('http://localhost:4000/users')
@@ -11,12 +13,20 @@ function Users() {
             .catch(error => console.error('Error fetching users:', error));
     }, []);
 
+    // Get current users
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
     return (
         <div>
             <h2><FormattedMessage id="users.title" /></h2>
             <div className="row">
-                {users.map(user => (
-                    <div className="col-md-4 mb-3" key={user.id}>
+                {currentUsers.map(user => (
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mb-3" key={user.id}>
                         <div className="card">
                             <div className="card-body">
                                 <h5 className="card-title">{user.name}</h5>
@@ -28,8 +38,36 @@ function Users() {
                     </div>
                 ))}
             </div>
+            <Pagination usersPerPage={usersPerPage} totalUsers={users.length} paginate={paginate} />
         </div>
     );
 }
 
 export default Users;
+
+function Pagination({ usersPerPage, totalUsers, paginate }) {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    const handleClick = (e, number) => {
+        e.preventDefault();  // Prevent the default anchor link behavior
+        paginate(number);
+    };
+
+    return (
+        <nav>
+            <ul className='pagination'>
+                {pageNumbers.map(number => (
+                    <li key={number} className='page-item'>
+                        <a onClick={(e) => handleClick(e, number)} href='!#' className='page-link'>
+                            {number}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
